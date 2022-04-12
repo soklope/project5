e5e9913a7194233fdeb033a40dc8ae2aac70333d
TrineBauneborg <95622942+TrineBauneborg@users.noreply.github.com>
2022-04-12 15:51:40 +0200

    Add files via upload
    compress img

--- slides.js
+++ slides.js
@@ -4,9 +4,9 @@ let i = 0;
 let images = [];	
 let time = 10000;	
 
-images[0] = "img/pic1.png";
-images[1] = "img/pic2.png";
-images[2] = "img/pic3.png";
+images[0] = "img/pic1.jpg";
+images[1] = "img/pic2.jpg";
+images[2] = "img/pic3.jpg";
 
 function changeImg(){
 	document.slide.src = images[i];
