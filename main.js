df5e54bf6049b6e89362d42f135681ff9e5f2295
AnnicaF <annif011098@gmail.com>
2022-04-19 15:53:02 +0200

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //* TRINE JS START *//
 function myFunction() {
     let x = document.getElementById("myLinks");
-   
+
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
@@ -19,4 +19,46 @@ function scrollFunction() {
   } else {
     document.getElementById("navbar").style.backgroundColor = "#00000000";
   }
+}
+
+//* Annica JS Start*//
+
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#sucess");
+const errorNodes = document.querySelectorAll("#.error");
+
+function validateForm(){
+
+clearMessages();
+
+if(nameInput.value.length < 1){
+  errorNodes[0].innerText = "Navn skal udfyldes";
+  nameInput.classList.add("error-border");
+}
+if(!emailIsValid(email.value)){
+  errorNodes[1].innerText = "Ugyldig email";
+  email.classList.add("error-border");
+}
+
+if(message.value.length < 1){
+  errorNodes[2].innerText = "Indtast din besked";
+  message.classList.add("error-border");
+}
+
+}
+
+function clearMessages(){
+  for(let i = 0; i < errorNodes.length; i++){
+    errorNodes[i].innerText = "";
+  }
+  nameInput.classList.remove("error-border");
+  email.classList.remove("error-border");
+  message.classList.remove("error-border");
+}
+
+function emailIsValid(email){
+  let pattern = /\S+@\S+/;
+  return pattern.test(email);
+
 }
